8e50db64854d2b57b272e3fcb24ef8298ce2e43b
Rodrigo Felix <rodrigo.felix321@gmail.com>
2026-01-16 22:07:14 -0300

    first commit

---
+++ src/main/java/com/felixsoftwares/credilsgestao/CrediLsGestaoApplication.java
@@ -0,0 +1,13 @@
+package com.felixsoftwares.credilsgestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrediLsGestaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrediLsGestaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/felixsoftwares/credilsgestao/CrediLsGestaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felixsoftwares.credilsgestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrediLsGestaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
